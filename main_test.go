cec6cbd78a8913d93860f1638a93b8912d9aace3
lrwx00t <lrwx00t@gmail.com>
2023-04-22 21:42:28 -0400

    refactor

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	. "lrwx00t/gocker/generator"
+)
+
+// TODO:
+// Formatter for single and double quotes
+// Multiline
+// runner/tester
+// remaining docker keywords
+
+func Example() {
+	GenerateDockerfile([]Dockerfile{
+		FROM("ubuntu"),
+		CMD("This is a sample comment"),
+		EXPOSE("8080"),
+		EXPOSE("8081"),
+		WORKDIR("/app"),
+	})
+	// Output:
+	// FROM ubuntu
+	// CMD This is a sample comment
+	// EXPOSE 8080
+	// EXPOSE 8081
+	// WORKDIR /app
+}
